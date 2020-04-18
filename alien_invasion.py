c1e1d13730fb1facde21c603173e3ad60d53be0d
zorrbass <john.steinbergrocketmail.com>
2020-04-18 14:23:49 +0200

    started core programm/ Class AlienInvasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,2 +1,29 @@
-"""this will be my own alien invasion game"""
-"""first real program ever!!! :)"""
+import sys
+import pygame
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior"""
+    def __init__(self):
+        """Initialize the game and create game resources"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Wolf's Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            # Watch for keyboard and mouse movements
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Make the most recently drawn screen visible
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    '''Make a game instance and run the game'''
+    ai = AlienInvasion()
+    ai.run_game()

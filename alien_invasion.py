ea95849e827a58e260bec6ff0550d9391435ea34
zorrbass <john.steinbergrocketmail.com>
2020-04-18 19:46:52 +0200

    added Ship and implemented in alien_invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -8,12 +9,17 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game and create game resources"""
         pygame.init()
+        # create an instance of class Settings
         self.settings = Settings()
 
+        # create a screen with parameters from setting
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_hight))
 
         pygame.display.set_caption("Wolf's Alien Invasion")
+
+        # Create an instance of class Ship
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -26,6 +32,7 @@ class AlienInvasion:
             # Redraw the screen during each pass through the loop
             # Sets the background color to bg_color
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible
             pygame.display.flip()

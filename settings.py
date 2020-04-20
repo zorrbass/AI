287ac100dd8d8fb67e27ee406fde1d158260779b
unknown <john.steinberg@rocketmail.com>
2020-04-20 12:21:59 +0200

    ship speed implmented in setting and ship

--- settings.py
+++ settings.py
@@ -6,3 +6,6 @@ class Settings:
         self.screen_width = 1200
         self.screen_hight = 800
         self.bg_color = (100, 120, 150)
+
+        # Ship settings
+        self.ship_speed = 0.8
--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and its starting position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # load the ship image and get its screen rect.
@@ -17,16 +18,22 @@ class Ship:
         # Start each new ship at the bottom center of the screen:
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store decimal value for the ship's horizontal position (self.rect.x)
+        self.x = float(self.rect.x)
+
         # Movement flag
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ships position based on the movement flag."""
+        # Update the ship's  x value not the rect which follows in the next line
         if self.moving_right:
-            self.rect.x += 1
-        elif self.moving_left:
-            self.rect.x -= 1
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
 
     def blitme(self):

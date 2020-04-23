e217d0c75b670320e863a63f96deeb5d1842e70d
unknown <john.steinberg@rocketmail.com>
2020-04-23 14:25:22 +0200

    made the fleet move

--- alien.py
+++ alien.py
@@ -9,6 +9,7 @@ class Alien(Sprite):
         """Initialize the alien and set it's starting position"""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         # Load the alien image and set it's rect attributes
         self.image = pygame.image.load("images/alien.bmp")
@@ -20,3 +21,15 @@ class Alien(Sprite):
 
         # Store the aliens exact horizontal position
         self.x = float(self.rect.x)
+
+    def check_edge(self):
+        """Return True if alien is at the edge of the screen"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
+    def update(self):
+        """Move the aliens to the right or left"""
+        self.x += (self.settings.alien_speed *
+                   self.settings.fleet_direction)
+        self.rect.x = self.x
--- settings.py
+++ settings.py
@@ -8,12 +8,19 @@ class Settings:
         self.bg_color = (100, 120, 150)
 
         # Ship settings
-        self.ship_speed = 0.4
+        self.ship_speed = 1.4
 
         # Bullet settings
-        self.bullet_speed = 0.5
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (55, 255, 0)
         self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed = 0.2
+        self.fleet_drop_speed = 10
+        # fleet direction 1 means right, -1 means left
+        self.fleet_direction = 1
+
 
